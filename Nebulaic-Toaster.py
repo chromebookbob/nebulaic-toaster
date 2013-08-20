eda8adc6428b002475a118dc13e7d7393297dec5
chromebookbob <chromebookbob@gmail.com>
2013-08-20 12:43:46 +0100

    File updated, more stars added

---
+++ Nebulaic-Toaster.py
@@ -0,0 +1,155 @@
+import random
+
+def starnear(name, computer_name):
+	stars = [star1]
+	random.choice(stars)(name, computer_name)
+	
+def starfar(name, computer_name):
+	stars = [stara, starb, starc, stard]
+	random.choice(stars)(name, computer_name)	
+
+def coordinates():
+	ac = random.uniform(-60, 60)
+	bc = random.uniform(-60, 60)
+	cc = random.uniform(-60, 60)
+	
+	
+	return u" %d\xb0 %d' %d\" " % (ac, bc, cc)
+	
+def star():
+	star_list = ["ACAMAR", "ACHERNAR", "Achird", "ACRUX", "Acubens", "ADARA", "Adhafera", "Adhil", "AGENA", "Ain-al-Rami", "Ain", "Aladfar", "Alathfar", "Albaldah", "Albali", "ALBIREO", "Alchiba", "ALCOR", "ALCYONE", "ALDEBARAN", "ALDERAMIN", "Aldhibah", "Alfecca-Meridiana", "Alfirk", "ALGENIB", "ALGIEBA", "ALGOL", "Algorab", "ALHENA", "ALIOTH", "ALKAID", "Alkalurops", "Alkes", "Alkurhah", "ALMAAK", "ALNAIR", "ALNATH", "ALNILAM", "ALNITAK", "Alniyat", "Alniyat", "ALPHARD", "ALPHEKKA", "ALPHERATZ", "Alrai", "Alrisha", "Alsafi", "Alsciaukat", "ALSHAIN", "Alshat", "Alsuhail", "ALTAIR", "Altarf", "Alterf", "Aludra", "Alula-Australis", "Alula-Borealis", "Alya", "Alzirr", "Ancha", "Angetenar", "ANKAA", "Anser", "ANTARES", "ARCTURUS", "Arkab-Posterior", "Arkab-Prior", "ARNEB", "Arrakis", "Ascella", "Asellus Australis", "Asellus-Borealis", "Asellus-Primus", "Asellus-Secondus", "Asellus-Tertius", "Asterope", "Atik", "Atlas", "Auva", "Avior", "Azelfafage", "Azha", "Azmidiske", "Baham", "Baten Kaitos", "Becrux", "Beid", "BELLATRIX", "BETELGEUSE", "Botein", "Brachium", "CANOPUS", "CAPELLA", "Caph", "CASTOR", "Cebalrai", "Celaeno", "Chara", "Chort", "COR-CAROLI", "Cursa", "Dabih", "DENEB", "DENEBOLA", "Dheneb", "Diadem", "DIPHDA", "Double-Double-(7051)", "Dschubba", "Dsiban", "DUBHE", "Ed-Asich", "Electra", "ELNATH", "ENIF", "ETAMIN", "FOMALHAUT", "Fornacis", "Fum-al-Samakah", "Furud", "Gacrux", "Gianfar", "Gienah-Cygni", "Gienah-Ghurab", "Gomeisa", "Gorgonea-Quarta", "Gorgonea-Secunda", "Gorgonea-Tertia", "Graffias", "Grafias", "Grumium", "HADAR", "Haedi", "HAMAL", "Hassaleh", "Head-of-Hydrus", "Herschel's-Garnet Star", "Heze", "Hoedus-II", "Homam", "Hyadum-I", "Hyadum-II", "IZAR", "Jabbah", "Kaffaljidhma", "Kajam", "KAUS-AUSTRALIS", "Kaus-Borealis", "Kaus-Meridionalis", "Keid", "Kitalpha", "KOCAB", "Kornephoros", "Kraz", "Kuma", "Lesath", "Maasym", "Maia", "Marfak", "Marfak", "Marfic", "Marfik", "MARKAB", "Matar", "Mebsuta", "MEGREZ", "Meissa", "Mekbuda", "Menkalinan", "MENKAR", "Menkar", "Menkent", "Menkib", "MERAK", "Merga", "Merope", "Mesarthim", "Metallah", "Miaplacidus", "Minkar", "MINTAKA", "MIRA", "MIRACH", "Miram", "MIRPHAK", "MIZAR", "Mufrid", "Muliphen", "Murzim", "Muscida", "Muscida", "Muscida", "Nair-al-Saif", "Naos", "Nash", "Nashira", "Nekkar", "NIHAL", "Nodus-Secundus", "NUNKI", "Nusakan", "Peacock", "PHAD", "Phaet", "Pherkad Minor", "Pherkad", "Pleione", "Polaris-Australis", "POLARIS", "POLLUX", "Porrima", "Praecipua", "Prima Giedi", "PROCYON", "Propus", "Propus", "Propus", "Rana", "Ras-Elased-Australis", "Ras-Elased-Borealis", "RASALGETHI", "RASALHAGUE", "Rastaban", "REGULUS", "Rigel-Kentaurus", "RIGEL", "Rijl-al-Awwa", "Rotanev", "Ruchba", "Ruchbah", "Rukbat", "Sabik", "Sadalachbia", "SADALMELIK", "Sadalsuud", "Sadr", "SAIPH", "Salm", "Sargas", "Sarin", "Sceptrum", "SCHEAT", "Secunda-Giedi", "Segin", "Seginus", "Sham", "Sharatan", "SHAULA", "SHEDIR", "Sheliak", "SIRIUS", "Situla", "Skat", "SPICA", "Sterope-II", "Sualocin", "Subra", "Suhail-al-Muhlif", "Sulafat", "Syrma", "Tabit-(1543)", "Tabit-(1544)", "Tabit-(1552)", "Tabit-(1570)", "Talitha", "Tania-Australis", "Tania-Borealis", "TARAZED", "Taygeta", "Tegmen", "Tejat-Posterior", "Terebellum", "Terebellum", "Terebellum", "Terebellum", "Thabit", "Theemim", "THUBAN", "Torcularis-Septentrionalis", "Turais", "Tyl", "UNUKALHAI", "VEGA", "VINDEMIATRIX", "Wasat", "Wezen", "Wezn", "Yed-Posterior", "Yed-Prior", "Yildun", "Zaniah", "Zaurak", "Zavijah", "Zibal", "Zosma", "Zuben-Elakrab, Zuben-Elakribi, Zuben-Elgenubi, Zuben-Elschemali"]
+	return random.choice(star_list)
+
+#print " Your coordinates are: %s Our nearest star is: %s " % (coordinates(), star())
+
+def action():
+	actions = [action1, action2, action3, action4]
+	random.choice(actions)()
+
+
+def begin():
+	print "-" * 20
+	print " ",
+	print "*" * 16
+	print "  NEBULAIC TOASTER"
+	print " ",
+	print "*" * 16
+	print "-" * 20
+	print """
+	  STARTING
+	  """
+	print "." * 50
+
+def jump(name, computer_name):
+
+	print """ Do you want to:
+	1. Jump to a nearby star
+	2. Jump to a far off star
+	3. Exit"""
+	answer = raw_input(">")
+	
+	if answer == "1":
+		starnear(name, computer_name)
+	elif answer == "2":
+		starfar(name, computer_name)
+	else:
+		print "I dont understand."
+		jump(name, computer_name)		
+
+def start():
+	name = raw_input("Captain's Name:")
+	print "Welcome, %s, you are the ship's captian. \n You will make decisions and guide our ship through space and beyond." % name
+	print "A few technicalities first captain. I will give you options to choose from regarding our path.\nAt this moment the ship's computer will only take numbers as answers."
+	print "By the way, you can give me a name"
+	computer_name = raw_input("What do you want to call me?")
+	print "From now on I shall be called %s." % computer_name
+	print " We will now go off, into the abyss..."
+	print "If you want to exit at any time just enter 3"
+	jump(name, computer_name)
+	return computer_name, name	
+
+
+		
+def star1(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "We are in orbit around the star %s." % star()
+	action()
+	
+def star2(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "I couldn't get us close enough to orbit around %s." % star()
+	print "Would you like to jump again?"
+	print "1. Yes"
+	print "2. No
+	answer = raw_input(">")
+	if answer == "1":
+		jump() 
+	elif answer == "2":
+		action()
+	elif answer == "3":
+		quit()	
+	else:
+		print "I couldn't catch that. Jumping anyway..."
+		jump()	
+	
+def star3(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "We are in orbit around the star %s." % star()
+	action()
+
+def star4(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "There are no stars around here %s." % name
+	action()
+
+def star5(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "We are in orbit around the star %s." % star()
+	action()
+
+def stara(name, computer_name):
+	print "REBOOTING INTO SAFE MODE"
+	print "-" * 20 
+	action()
+	
+def starb(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "We are in orbit around the star %s." % star()
+	action()
+
+def starc(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "Our current coordinates are: %s." % coordinates()
+	print "We are in orbit around the star %s." % star()
+	action()
+		
+def stard(name, computer_name):
+	print "Hello %s, it's %s here." % (name, computer_name)
+	print "I cant give you any accurate coordinates right now, %s, we are in deep space." % name
+	print "I managed to find our nearest star: %s." % star()
+	action()		
+def quit():
+	print "Are you sure you want to quit?"
+	print "1. Yes"
+	print "2. No, restart"
+	answer = raw_input(">")
+	if answer == 1:
+		exit(0)
+	else:
+		game()
+				
+def game():
+	begin()	
+	start()
+	
+game()
+
+
+	
+	
